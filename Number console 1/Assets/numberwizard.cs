c242ed3ccd1aa453b105bdf59dcdeadc476d537b
Vishal singh <44376633+Vishal14H@users.noreply.github.com>
2020-08-29 23:14:25 +0530

    Simple Wizard Game

--- Number console 1/Assets/numberwizard.cs
+++ Number console 1/Assets/numberwizard.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class numberwizard : MonoBehaviour
 {
-    int max = 1000;
-    int min = 1;
-    int guess = 500;
+    int max ;
+    int min ;
+    int guess ;
 
 
     // Start is called before the first frame update
     void Start()
     {
+        StartGame();
         
+        
+        
+    }
+    void StartGame(){
+
+        max = 1000;
+        min = 1;
+        guess = 500;
         Debug.Log("Hey, Let's Play Number Wizard! ");
         Debug.Log("Pick a number , and don't tell to me !");
         Debug.Log("Highest Number you can take is:" + max);
         Debug.Log("Lowest number you can take is:" + min);
         Debug.Log("Tell me that your number is greater or smaller than:" + guess);
-         Debug.Log("Push Up = Higher , Push Down = Lower, Push Enter = Correct");
-         max = max + 1 ;
-        
-    }
+        Debug.Log("Push Up = Higher , Push Down = Lower, Push Enter = Correct");
+        max = max + 1 ;
 
+    }
 
     // Update is called once per frame
     void Update()
@@ -30,18 +38,22 @@ public class numberwizard : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.UpArrow))
         {
            min = guess;
-           guess = ( max + min ) / 2;
-           Debug.Log("Is it Higher or Lower than....." + guess);
+           NextGuess();
         }
         else if (Input.GetKeyDown(KeyCode.DownArrow)){
            
             max = guess;
-            guess = (max + min) / 2;
-            Debug.Log("Is it Higher or Lower than..... "+ guess);
+            NextGuess();
     
         }
         else if (Input.GetKeyDown(KeyCode.Return)){
             Debug.Log("I know i am a Genius.");
+            StartGame();
         }
+    }
+
+    void NextGuess(){
+        guess = (max + min ) / 2;
+        Debug.Log("Is the number Higher or Lower than ......" + guess);
     }
 }
