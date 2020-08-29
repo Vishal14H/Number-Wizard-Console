0b356a291c94bae83bcbc28f5807a76915a960d8
Vishal singh <44376633+Vishal14H@users.noreply.github.com>
2020-08-29 08:03:41 +0530

    Input to Take by player.

---
+++ Number console 1/Assets/numberwizard.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class numberwizard : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int max = 1000;
+        int min = 1;
+        Debug.Log("Welcome to Number Wizard !");
+        Debug.Log("Pick a number , and don't tell to me !");
+        Debug.Log("Highest Number you can take is:" + max);
+        Debug.Log("Lowest number you can take is:" + min);
+        Debug.Log("Tell me that your number is greater or smaller than my 500! ");
+         Debug.Log("Push Up = Higher , Push Down = Lower, Push Enter = Correct");
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Debug.Log("Up Arrow key was pressed.");
+        }
+        if (Input.GetKeyDown(KeyCode.DownArrow)){
+            Debug.Log("Down Arrow key was pressed.");
+    
+        }
+        if (Input.GetKeyDown(KeyCode.Return)){
+            Debug.Log("You hit Enter.");
+        }
+    }
+}
